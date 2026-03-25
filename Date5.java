4f16592820bcf45fccbe98b3d788e95396a6ffba
dayul615 <dayul9888@gmail.com>
2026-03-25 22:29:59 +0900

    if문
    java

---
+++ Date5.java
@@ -0,0 +1,17 @@
+package eaxm;
+
+public class Date5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+		int num2 = 20;
+
+		if (num > 5 && num2 > 15) {
+		    System.out.println("둘 다 조건 만족");
+		} else {
+		    System.out.println("조건 불만족");
+		}
+	}
+
+}
