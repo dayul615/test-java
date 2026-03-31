3b6cfb3077bafe3034fb01614260edfaca7b1d00
dayul615 <dayul9888@gmail.com>
2026-03-31 23:43:32 +0900

    10단홀짝출력.java

---
+++ Num.java
@@ -0,0 +1,17 @@
+package eaxm;
+
+public class Num {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i + " : 짝수입니다");
+            } else {
+                System.out.println(i + " : 홀수입니다");
+            }
+
+        }
+
+    }
+}
